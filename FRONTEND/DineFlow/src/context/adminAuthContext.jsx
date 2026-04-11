671fdde1c08e4b258c352773529499a9d8caf8bf
Akash P Anand <akashpanand007@gmail.com>
2026-04-11 18:14:39 +0530

    fix admin auth initialization

--- FRONTEND/DineFlow/src/context/adminAuthContext.jsx
+++ FRONTEND/DineFlow/src/context/adminAuthContext.jsx
@@ -1,29 +1,31 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import API from "../api/api";
 
-
-
 const AdminAuthContext = createContext();
 
 export const AdminAuthProvider = ({ children }) => {
   const [admin, setAdmin] = useState(null);
   const [loading, setLoading] = useState(true);
 
- const checkAuth = async () => {
-  try {
-    const res = await API.get("/admin/me");
-    setAdmin(res.data.admin);
-  } catch {
-    setAdmin(null);
-  } finally {
-    setLoading(false);
-  }
-};
+  const checkAuth = async () => {
+    try {
+      const res = await API.get("/admin/me");
+      setAdmin(res.data.admin);
+    } catch {
+      setAdmin(null);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-const logout = async () => {
-  await API.post("/admin/logout");
-  setAdmin(null);
-};
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  const logout = async () => {
+    await API.post("/admin/logout");
+    setAdmin(null);
+  };
 
   return (
     <AdminAuthContext.Provider value={{ admin, loading, checkAuth, logout }}>
